0cee6ce55bc48dff648b6d596b1e274c194f243d
CharlesCNorton <machineelv@gmail.com>
2026-03-08 15:21:24 -0400

    tests: fix gmp ci and strengthen todo wips

--- tests/wip/todo_nested_module_type/todo_nested_module_type.t.cpp
+++ tests/wip/todo_nested_module_type/todo_nested_module_type.t.cpp
@@ -1,8 +1,11 @@
 // Copyright 2026 Bloomberg Finance L.P.
 // Distributed under the terms of the GNU LGPL v2.1 license.
+#include <cassert>
+
 #include "todo_nested_module_type.h"
 
 int main()
 {
+    assert(TodoNestedModuleType::test_twice == 2u);
     return 0;
 }
--- tests/wip/todo_with_type_constraint/todo_with_type_constraint.t.cpp
+++ tests/wip/todo_with_type_constraint/todo_with_type_constraint.t.cpp
@@ -1,8 +1,11 @@
 // Copyright 2026 Bloomberg Finance L.P.
 // Distributed under the terms of the GNU LGPL v2.1 license.
+#include <cassert>
+
 #include "todo_with_type_constraint.h"
 
 int main()
 {
+    assert(TodoWithTypeConstraint::test_twice == 2u);
     return 0;
 }
