765c531fb93d044c67505cc3292bc09997992ec1
Joomy Korkut <jkorkut@bloomberg.net>
2026-03-13 08:58:33 -0400

    Fix CI

--- tests/basics/mutual_recursion/mutual_recursion.cpp
+++ tests/basics/mutual_recursion/mutual_recursion.cpp
@@ -11,120 +11,54 @@
 #include <string>
 #include <variant>
 
-bool MutualRecursion::even(const unsigned int n) {
+bool MutualRecursion::is_even(const unsigned int n) {
   if (n <= 0) {
     return true;
   } else {
-    unsigned int n_ = n - 1;
-    return odd(n_);
+    unsigned int m = n - 1;
+    return is_odd(m);
   }
 }
 
-bool MutualRecursion::odd(const unsigned int n) {
+bool MutualRecursion::is_odd(const unsigned int n) {
   if (n <= 0) {
     return false;
   } else {
-    unsigned int n_ = n - 1;
-    return even(n_);
+    unsigned int m = n - 1;
+    return is_even(m);
   }
 }
 
-unsigned int MutualRecursion::sum_even_indices(const unsigned int n,
-                                               const unsigned int acc) {
-  if (n <= 0) {
-    return std::move(acc);
-  } else {
-    unsigned int n_ = n - 1;
-    return sum_odd_indices(std::move(n_), acc);
-  }
-}
-
-unsigned int MutualRecursion::sum_odd_indices(const unsigned int n,
-                                              const unsigned int acc) {
-  if (n <= 0) {
-    return std::move(acc);
-  } else {
-    unsigned int n_ = n - 1;
-    return sum_even_indices(std::move(n_), (acc + n));
-  }
-}
-
-unsigned int MutualRecursion::process_a(const unsigned int n,
-                                        const unsigned int m) {
-  if (n <= 0) {
-    return std::move(m);
-  } else {
-    unsigned int n_ = n - 1;
-    return (process_b(std::move(n_), m) + n);
-  }
-}
-
-unsigned int MutualRecursion::process_b(const unsigned int n,
-                                        const unsigned int m) {
-  if (n <= 0) {
-    return std::move(m);
-  } else {
-    unsigned int n_ = n - 1;
-    return (process_a(std::move(n_), m) + m);
-  }
-}
-
-unsigned int
-MutualRecursion::eval_expr(const std::shared_ptr<MutualRecursion::expr> &e) {
+unsigned int MutualRecursion::tree_sum(
+    const std::shared_ptr<MutualRecursion::tree<unsigned int>> &t) {
   return std::visit(
       Overloaded{
-          [](const typename MutualRecursion::expr::Val _args) -> unsigned int {
+          [](const typename MutualRecursion::tree<unsigned int>::Leaf _args)
+              -> unsigned int {
             unsigned int n = _args.d_a0;
             return std::move(n);
           },
-          [](const typename MutualRecursion::expr::BinOp _args)
+          [](const typename MutualRecursion::tree<unsigned int>::Node _args)
               -> unsigned int {
-            unsigned int op = _args.d_a0;
-            std::shared_ptr<MutualRecursion::expr> l = _args.d_a1;
-            std::shared_ptr<MutualRecursion::expr> r = _args.d_a2;
-            if (op <= 0) {
-              return (eval_expr(std::move(l)) + eval_expr(std::move(r)));
-            } else {
-              unsigned int _x = op - 1;
-              return (eval_expr(std::move(l)) * eval_expr(std::move(r)));
-            }
-          },
-          [](const typename MutualRecursion::expr::UnOp _args) -> unsigned int {
-            unsigned int op = _args.d_a0;
-            std::shared_ptr<MutualRecursion::expr> e_ = _args.d_a1;
-            if (op <= 0) {
-              return eval_expr(std::move(e_));
-            } else {
-              unsigned int _x = op - 1;
-              return 0u;
-            }
+            std::shared_ptr<MutualRecursion::forest<unsigned int>> f =
+                _args.d_a0;
+            return forest_sum(std::move(f));
           }},
-      e->v());
+      t->v());
 }
 
-unsigned int MutualRecursion::f1(const unsigned int n) {
-  if (n <= 0) {
-    return 0u;
-  } else {
-    unsigned int n_ = n - 1;
-    return (1u + f2(n_));
-  }
-}
-
-unsigned int MutualRecursion::f2(const unsigned int n) {
-  if (n <= 0) {
-    return 0u;
-  } else {
-    unsigned int n_ = n - 1;
-    return (2u + f3(n_));
-  }
-}
-
-unsigned int MutualRecursion::f3(const unsigned int n) {
-  if (n <= 0) {
-    return 0u;
-  } else {
-    unsigned int n_ = n - 1;
-    return (3u + f1(n_));
-  }
+unsigned int MutualRecursion::forest_sum(
+    const std::shared_ptr<MutualRecursion::forest<unsigned int>> &f) {
+  return std::visit(
+      Overloaded{
+          [](const typename MutualRecursion::forest<unsigned int>::Empty _args)
+              -> unsigned int { return 0u; },
+          [](const typename MutualRecursion::forest<unsigned int>::Trees _args)
+              -> unsigned int {
+            std::shared_ptr<MutualRecursion::tree<unsigned int>> t = _args.d_a0;
+            std::shared_ptr<MutualRecursion::forest<unsigned int>> rest =
+                _args.d_a1;
+            return (tree_sum(std::move(t)) + forest_sum(std::move(rest)));
+          }},
+      f->v());
 }
--- tests/basics/mutual_recursion/mutual_recursion.h
+++ tests/basics/mutual_recursion/mutual_recursion.h
@@ -21,78 +21,53 @@ template <class... Ts> struct Overloaded : Ts... {
 template <class... Ts> Overloaded(Ts...) -> Overloaded<Ts...>;
 
 struct MutualRecursion {
-  static bool even(const unsigned int n);
-  static bool odd(const unsigned int n);
-  static unsigned int sum_even_indices(const unsigned int n,
-                                       const unsigned int acc);
-  static unsigned int sum_odd_indices(const unsigned int n,
-                                      const unsigned int acc);
-  static unsigned int process_a(const unsigned int n, const unsigned int m);
-  static unsigned int process_b(const unsigned int n, const unsigned int m);
+  static bool is_even(const unsigned int n);
+  static bool is_odd(const unsigned int n);
+  template <typename t_A> struct tree;
+  template <typename t_A> struct forest;
 
-  struct expr {
+  template <typename t_A> struct tree {
     // TYPES
-    struct Val {
-      unsigned int d_a0;
+    struct Leaf {
+      t_A d_a0;
     };
 
-    struct BinOp {
-      unsigned int d_a0;
-      std::shared_ptr<expr> d_a1;
-      std::shared_ptr<expr> d_a2;
+    struct Node {
+      std::shared_ptr<forest<t_A>> d_a0;
     };
 
-    struct UnOp {
-      unsigned int d_a0;
-      std::shared_ptr<expr> d_a1;
-    };
-
-    using variant_t = std::variant<Val, BinOp, UnOp>;
+    using variant_t = std::variant<Leaf, Node>;
 
   private:
     // DATA
     variant_t d_v_;
 
     // CREATORS
-    explicit expr(Val _v) : d_v_(std::move(_v)) {}
+    explicit tree(Leaf _v) : d_v_(std::move(_v)) {}
 
-    explicit expr(BinOp _v) : d_v_(std::move(_v)) {}
-
-    explicit expr(UnOp _v) : d_v_(std::move(_v)) {}
+    explicit tree(Node _v) : d_v_(std::move(_v)) {}
 
   public:
     // TYPES
     struct ctor {
       ctor() = delete;
 
-      static std::shared_ptr<expr> Val_(unsigned int a0) {
-        return std::shared_ptr<expr>(new expr(Val{a0}));
+      static std::shared_ptr<tree<t_A>> Leaf_(t_A a0) {
+        return std::shared_ptr<tree<t_A>>(new tree<t_A>(Leaf{a0}));
       }
 
-      static std::shared_ptr<expr> BinOp_(unsigned int a0,
-                                          const std::shared_ptr<expr> &a1,
-                                          const std::shared_ptr<expr> &a2) {
-        return std::shared_ptr<expr>(new expr(BinOp{a0, a1, a2}));
+      static std::shared_ptr<tree<t_A>>
+      Node_(const std::shared_ptr<forest<t_A>> &a0) {
+        return std::shared_ptr<tree<t_A>>(new tree<t_A>(Node{a0}));
       }
 
-      static std::shared_ptr<expr> UnOp_(unsigned int a0,
-                                         const std::shared_ptr<expr> &a1) {
-        return std::shared_ptr<expr>(new expr(UnOp{a0, a1}));
+      static std::unique_ptr<tree<t_A>> Leaf_uptr(t_A a0) {
+        return std::unique_ptr<tree<t_A>>(new tree<t_A>(Leaf{a0}));
       }
 
-      static std::unique_ptr<expr> Val_uptr(unsigned int a0) {
-        return std::unique_ptr<expr>(new expr(Val{a0}));
-      }
-
-      static std::unique_ptr<expr> BinOp_uptr(unsigned int a0,
-                                              const std::shared_ptr<expr> &a1,
-                                              const std::shared_ptr<expr> &a2) {
-        return std::unique_ptr<expr>(new expr(BinOp{a0, a1, a2}));
-      }
-
-      static std::unique_ptr<expr> UnOp_uptr(unsigned int a0,
-                                             const std::shared_ptr<expr> &a1) {
-        return std::unique_ptr<expr>(new expr(UnOp{a0, a1}));
+      static std::unique_ptr<tree<t_A>>
+      Node_uptr(const std::shared_ptr<forest<t_A>> &a0) {
+        return std::unique_ptr<tree<t_A>>(new tree<t_A>(Node{a0}));
       }
     };
 
@@ -103,67 +78,176 @@ struct MutualRecursion {
     const variant_t &v() const { return d_v_; }
   };
 
-  template <typename T1, MapsTo<T1, unsigned int> F0,
-            MapsTo<T1, unsigned int, std::shared_ptr<expr>, T1,
-                   std::shared_ptr<expr>, T1>
-                F1,
-            MapsTo<T1, unsigned int, std::shared_ptr<expr>, T1> F2>
-  static T1 expr_rect(F0 &&f, F1 &&f0, F2 &&f4,
-                      const std::shared_ptr<expr> &e) {
+  template <typename t_A> struct forest {
+    // TYPES
+    struct Empty {};
+
+    struct Trees {
+      std::shared_ptr<tree<t_A>> d_a0;
+      std::shared_ptr<forest<t_A>> d_a1;
+    };
+
+    using variant_t = std::variant<Empty, Trees>;
+
+  private:
+    // DATA
+    variant_t d_v_;
+
+    // CREATORS
+    explicit forest(Empty _v) : d_v_(std::move(_v)) {}
+
+    explicit forest(Trees _v) : d_v_(std::move(_v)) {}
+
+  public:
+    // TYPES
+    struct ctor {
+      ctor() = delete;
+
+      static std::shared_ptr<forest<t_A>> Empty_() {
+        return std::shared_ptr<forest<t_A>>(new forest<t_A>(Empty{}));
+      }
+
+      static std::shared_ptr<forest<t_A>>
+      Trees_(const std::shared_ptr<tree<t_A>> &a0,
+             const std::shared_ptr<forest<t_A>> &a1) {
+        return std::shared_ptr<forest<t_A>>(new forest<t_A>(Trees{a0, a1}));
+      }
+
+      static std::unique_ptr<forest<t_A>> Empty_uptr() {
+        return std::unique_ptr<forest<t_A>>(new forest<t_A>(Empty{}));
+      }
+
+      static std::unique_ptr<forest<t_A>>
+      Trees_uptr(const std::shared_ptr<tree<t_A>> &a0,
+                 const std::shared_ptr<forest<t_A>> &a1) {
+        return std::unique_ptr<forest<t_A>>(new forest<t_A>(Trees{a0, a1}));
+      }
+    };
+
+    // MANIPULATORS
+    variant_t &v_mut() { return d_v_; }
+
+    // ACCESSORS
+    const variant_t &v() const { return d_v_; }
+  };
+
+  template <typename T1, typename T2, MapsTo<T2, T1> F0,
+            MapsTo<T2, std::shared_ptr<forest<T1>>> F1>
+  static T2 tree_rect(F0 &&f, F1 &&f0, const std::shared_ptr<tree<T1>> &t) {
     return std::visit(
-        Overloaded{[&](const typename expr::Val _args) -> T1 {
-                     unsigned int n = _args.d_a0;
-                     return f(std::move(n));
+        Overloaded{[&](const typename tree<T1>::Leaf _args) -> T2 {
+                     T1 a = _args.d_a0;
+                     return f(a);
                    },
-                   [&](const typename expr::BinOp _args) -> T1 {
-                     unsigned int n = _args.d_a0;
-                     std::shared_ptr<expr> e0 = _args.d_a1;
-                     std::shared_ptr<expr> e1 = _args.d_a2;
-                     return f0(std::move(n), e0, expr_rect<T1>(f, f0, f4, e0),
-                               e1, expr_rect<T1>(f, f0, f4, e1));
-                   },
-                   [&](const typename expr::UnOp _args) -> T1 {
-                     unsigned int n = _args.d_a0;
-                     std::shared_ptr<expr> e0 = _args.d_a1;
-                     return f4(std::move(n), e0, expr_rect<T1>(f, f0, f4, e0));
+                   [&](const typename tree<T1>::Node _args) -> T2 {
+                     std::shared_ptr<forest<T1>> f1 = _args.d_a0;
+                     return f0(std::move(f1));
                    }},
-        e->v());
+        t->v());
   }
 
-  template <typename T1, MapsTo<T1, unsigned int> F0,
-            MapsTo<T1, unsigned int, std::shared_ptr<expr>, T1,
-                   std::shared_ptr<expr>, T1>
-                F1,
-            MapsTo<T1, unsigned int, std::shared_ptr<expr>, T1> F2>
-  static T1 expr_rec(F0 &&f, F1 &&f0, F2 &&f4, const std::shared_ptr<expr> &e) {
+  template <typename T1, typename T2, MapsTo<T2, T1> F0,
+            MapsTo<T2, std::shared_ptr<forest<T1>>> F1>
+  static T2 tree_rec(F0 &&f, F1 &&f0, const std::shared_ptr<tree<T1>> &t) {
     return std::visit(
-        Overloaded{[&](const typename expr::Val _args) -> T1 {
-                     unsigned int n = _args.d_a0;
-                     return f(std::move(n));
+        Overloaded{[&](const typename tree<T1>::Leaf _args) -> T2 {
+                     T1 a = _args.d_a0;
+                     return f(a);
                    },
-                   [&](const typename expr::BinOp _args) -> T1 {
-                     unsigned int n = _args.d_a0;
-                     std::shared_ptr<expr> e0 = _args.d_a1;
-                     std::shared_ptr<expr> e1 = _args.d_a2;
-                     return f0(std::move(n), e0, expr_rec<T1>(f, f0, f4, e0),
-                               e1, expr_rec<T1>(f, f0, f4, e1));
-                   },
-                   [&](const typename expr::UnOp _args) -> T1 {
-                     unsigned int n = _args.d_a0;
-                     std::shared_ptr<expr> e0 = _args.d_a1;
-                     return f4(std::move(n), e0, expr_rec<T1>(f, f0, f4, e0));
+                   [&](const typename tree<T1>::Node _args) -> T2 {
+                     std::shared_ptr<forest<T1>> f1 = _args.d_a0;
+                     return f0(std::move(f1));
                    }},
-        e->v());
+        t->v());
   }
 
-  static unsigned int eval_expr(const std::shared_ptr<expr> &e);
-  static unsigned int f1(const unsigned int n);
-  static unsigned int f2(const unsigned int n);
-  static unsigned int f3(const unsigned int n);
-  static inline const bool test_even = even(10u);
-  static inline const unsigned int test_sum = sum_even_indices(5u, 0u);
-  static inline const unsigned int test_eval = eval_expr(
-      expr::ctor::BinOp_(0u, expr::ctor::Val_(5u), expr::ctor::Val_(10u)));
+  template <
+      typename T1, typename T2,
+      MapsTo<T2, std::shared_ptr<tree<T1>>, std::shared_ptr<forest<T1>>, T2> F1>
+  static T2 forest_rect(const T2 f, F1 &&f0,
+                        const std::shared_ptr<forest<T1>> &f1) {
+    return std::visit(
+        Overloaded{
+            [&](const typename forest<T1>::Empty _args) -> T2 { return f; },
+            [&](const typename forest<T1>::Trees _args) -> T2 {
+              std::shared_ptr<tree<T1>> t = _args.d_a0;
+              std::shared_ptr<forest<T1>> f2 = _args.d_a1;
+              return f0(std::move(t), f2, forest_rect<T1, T2>(f, f0, f2));
+            }},
+        f1->v());
+  }
+
+  template <
+      typename T1, typename T2,
+      MapsTo<T2, std::shared_ptr<tree<T1>>, std::shared_ptr<forest<T1>>, T2> F1>
+  static T2 forest_rec(const T2 f, F1 &&f0,
+                       const std::shared_ptr<forest<T1>> &f1) {
+    return std::visit(
+        Overloaded{
+            [&](const typename forest<T1>::Empty _args) -> T2 { return f; },
+            [&](const typename forest<T1>::Trees _args) -> T2 {
+              std::shared_ptr<tree<T1>> t = _args.d_a0;
+              std::shared_ptr<forest<T1>> f2 = _args.d_a1;
+              return f0(std::move(t), f2, forest_rec<T1, T2>(f, f0, f2));
+            }},
+        f1->v());
+  }
+
+  template <typename T1>
+  static unsigned int tree_size(const std::shared_ptr<tree<T1>> &t) {
+    return std::visit(
+        Overloaded{[](const typename tree<T1>::Leaf _args) -> unsigned int {
+                     return 1u;
+                   },
+                   [](const typename tree<T1>::Node _args) -> unsigned int {
+                     std::shared_ptr<forest<T1>> f = _args.d_a0;
+                     return forest_size<T1>(std::move(f));
+                   }},
+        t->v());
+  }
+
+  template <typename T1>
+  static unsigned int forest_size(const std::shared_ptr<forest<T1>> &f) {
+    return std::visit(
+        Overloaded{[](const typename forest<T1>::Empty _args) -> unsigned int {
+                     return 0u;
+                   },
+                   [](const typename forest<T1>::Trees _args) -> unsigned int {
+                     std::shared_ptr<tree<T1>> t = _args.d_a0;
+                     std::shared_ptr<forest<T1>> rest = _args.d_a1;
+                     return (tree_size<T1>(std::move(t)) +
+                             forest_size<T1>(std::move(rest)));
+                   }},
+        f->v());
+  }
+
+  static unsigned int tree_sum(const std::shared_ptr<tree<unsigned int>> &t);
+  static unsigned int
+  forest_sum(const std::shared_ptr<forest<unsigned int>> &f);
+  static inline const bool test_even_0 = is_even(0u);
+  static inline const bool test_even_4 = is_even(4u);
+  static inline const bool test_odd_3 = is_odd(3u);
+  static inline const bool test_odd_4 = is_odd(4u);
+  static inline const std::shared_ptr<tree<unsigned int>> simple_tree =
+      tree<unsigned int>::ctor::Node_(forest<unsigned int>::ctor::Trees_(
+          tree<unsigned int>::ctor::Leaf_(1u),
+          forest<unsigned int>::ctor::Trees_(
+              tree<unsigned int>::ctor::Leaf_(2u),
+              forest<unsigned int>::ctor::Empty_())));
+  static inline const std::shared_ptr<tree<unsigned int>> nested_tree =
+      tree<unsigned int>::ctor::Node_(forest<unsigned int>::ctor::Trees_(
+          tree<unsigned int>::ctor::Node_(forest<unsigned int>::ctor::Trees_(
+              tree<unsigned int>::ctor::Leaf_(3u),
+              forest<unsigned int>::ctor::Empty_())),
+          forest<unsigned int>::ctor::Trees_(
+              tree<unsigned int>::ctor::Leaf_(4u),
+              forest<unsigned int>::ctor::Empty_())));
+  static inline const unsigned int test_size_simple =
+      tree_size<unsigned int>(simple_tree);
+  static inline const unsigned int test_size_nested =
+      tree_size<unsigned int>(nested_tree);
+  static inline const unsigned int test_sum_simple = tree_sum(simple_tree);
+  static inline const unsigned int test_sum_nested = tree_sum(nested_tree);
 };
 
 #endif // INCLUDED_MUTUAL_RECURSION
