0e85f8b3b5d624b5f100d6de3ebbfea11a265528
Joomy Korkut <jkorkut@bloomberg.net>
2026-05-16 06:36:50 -0400

    Delete unnecessary files

--- optional_self_deep_copy.cpp
+++
@@ -1,6 +0,0 @@
-#include <optional_self_deep_copy.h>
-
-std::pair<OptionalSelfDeepCopy::chain, OptionalSelfDeepCopy::chain>
-OptionalSelfDeepCopy::dup_chain(OptionalSelfDeepCopy::chain c) {
-  return std::make_pair(c, c);
-}
