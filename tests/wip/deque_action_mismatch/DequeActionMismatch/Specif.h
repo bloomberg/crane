2f225c17312984a6bba7aec4d0209512a9240945
Joomy Korkut <jkorkut@bloomberg.net>
2026-06-19 20:34:51 -0400

    New wip test

---
+++ tests/wip/deque_action_mismatch/DequeActionMismatch/Specif.h
@@ -0,0 +1,22 @@
+#ifndef INCLUDED_SPECIF
+#define INCLUDED_SPECIF
+
+#include <utility>
+
+namespace Specif {
+
+template <typename A, typename P> struct SigT {
+  // DATA
+  A x;
+  P a1;
+
+  // ACCESSORS
+  SigT<A, P> clone() const { return {x, a1}; }
+
+  // CREATORS
+  static SigT<A, P> existt(A x, P a1) { return {std::move(x), std::move(a1)}; }
+};
+
+} // namespace Specif
+
+#endif // INCLUDED_SPECIF
