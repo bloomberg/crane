62fd5f94bce5b91503e7f31cbafe74ea5c833ffd
Joomy Korkut <jkorkut@bloomberg.net>
2026-08-29 07:31:12 -0400

    Promote eta_closure_fixpoint to the regression suite
    The Fixpoint form of the first-class-closure arity bug: the earlier currying
    of a flat-declared global handed over as a value covers it too.

--- tests/regression/eta_closure_fixpoint/eta_closure_fixpoint.cpp
+++ tests/regression/eta_closure_fixpoint/eta_closure_fixpoint.cpp
@@ -18,7 +18,9 @@ uint64_t EtaClosureFixpoint::run(uint64_t k) {
              List<uint64_t>::cons(
                  UINT64_C(2),
                  List<uint64_t>::cons(UINT64_C(3), List<uint64_t>::nil())))
-      .template map<std::function<uint64_t(uint64_t)>>(adder)
+      .template map<std::function<uint64_t(uint64_t)>>([](uint64_t _ec0) {
+        return [=](uint64_t _ec1) mutable { return adder(_ec0, _ec1); };
+      })
       .template fold_left<uint64_t>(
           [](uint64_t a, std::function<uint64_t(uint64_t)> f) { return f(a); },
           k);
--- tests/regression/eta_closure_fixpoint/eta_closure_fixpoint.h
+++ tests/regression/eta_closure_fixpoint/eta_closure_fixpoint.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDED_ETA_CLOSURE_FIXPOINT
 #define INCLUDED_ETA_CLOSURE_FIXPOINT
 
+#include "crane_fn.h"
 #include "small_vector.h"
 #include <any>
 #include <atomic>
@@ -112,23 +113,42 @@ public:
   template <typename T1, typename F0>
     requires std::is_invocable_r_v<T1, F0 &, T1 &, A &>
   T1 fold_left(F0 &&f, T1 a0) const {
-    if (std::holds_alternative<typename List<A>::Nil>(this->v())) {
-      return a0;
-    } else {
-      const auto &[a1, a2] = std::get<typename List<A>::Cons>(this->v());
-      return a2->template fold_left<T1>(f, f(a0, a1));
+    const List *_loop_self = this;
+    T1 _loop_a0 = std::move(a0);
+    while (true) {
+      auto &&_sv = *_loop_self;
+      if (std::holds_alternative<typename List<A>::Nil>(_sv.v())) {
+        return _loop_a0;
+      } else {
+        const auto &[a1, a2] = std::get<typename List<A>::Cons>(_sv.v());
+        _loop_self = crane_raw(a2);
+        _loop_a0 = f(std::move(_loop_a0), a1);
+      }
     }
   }
 
   template <typename T1, typename F0>
     requires std::is_invocable_r_v<T1, F0 &, A &>
   List<T1> map(F0 &&f) const {
-    if (std::holds_alternative<typename List<A>::Nil>(this->v())) {
-      return List<T1>::nil();
-    } else {
-      const auto &[a0, a1] = std::get<typename List<A>::Cons>(this->v());
-      return List<T1>::cons(f(a0), a1->template map<T1>(f));
+    std::shared_ptr<List<T1>> _head{};
+    std::shared_ptr<List<T1>> *_write = &_head;
+    const List *_loop_self = this;
+    while (true) {
+      auto &&_sv = *_loop_self;
+      if (std::holds_alternative<typename List<A>::Nil>(_sv.v())) {
+        *_write = std::make_shared<List<T1>>(List<T1>::nil());
+        break;
+      } else {
+        const auto &[a0, a1] = std::get<typename List<A>::Cons>(_sv.v());
+        auto _cell =
+            std::make_shared<List<T1>>(typename List<T1>::Cons(f(a0), nullptr));
+        *_write = std::move(_cell);
+        _write = &std::get<typename List<T1>::Cons>((*_write)->v_mut()).l;
+        _loop_self = crane_raw(a1);
+        continue;
+      }
     }
+    return std::move(*_head);
   }
 };
 
