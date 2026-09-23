251bc564bb6bd5de9770329f496c8e79306e4ab8
Joomy Korkut <jkorkut@bloomberg.net>
2026-09-22 22:40:00 -0400

    Move tfunctor_record_field_carrier to the regression folder

--- tests/regression/tfunctor_record_field_carrier/tfunctor_record_field_carrier.cpp
+++ tests/regression/tfunctor_record_field_carrier/tfunctor_record_field_carrier.cpp
@@ -1,10 +1,15 @@
 #include "tfunctor_record_field_carrier.h"
 
+template <typename _CraneTcArg>
+using _crane_carrier_tc = std::optional<Exp<_CraneTcArg>>;
+template <typename _CraneTcArg>
+using _crane_carrier_tc1 = List<Exp<_CraneTcArg>>;
+
 glob<std::any> TFunctor_glob(std::function<std::any(std::any)> f,
                              const glob<std::any> &g) {
   return glob<std::any>{
       f(g.g_name),
-      tfmap<std::optional>(
+      tfmap<_crane_carrier_tc>(
           []() {
             return [](std::function<std::any(std::any)> _x0,
                       std::optional<Exp<std::any>> _x1)
@@ -17,7 +22,7 @@ glob<std::any> TFunctor_glob(std::function<std::any(std::any)> f,
             };
           }(),
           f, g.g_exp),
-      tfmap<List>(
+      tfmap<_crane_carrier_tc1>(
           []() {
             return [](std::function<std::any(std::any)> _x0,
                       List<Exp<std::any>> _x1) -> List<Exp<std::any>> {
